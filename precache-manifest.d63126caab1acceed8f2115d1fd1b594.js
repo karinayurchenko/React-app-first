b2664c09383ca3f49ac3b2a13f950454c42f7353
karinayurchenko <karina.yurchenko28@gmail.com>
2020-04-12 16:01:44 +0300

    Updates

---
+++ precache-manifest.d63126caab1acceed8f2115d1fd1b594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae5bf4c14b2adcbe9c2d4409d44fd6f",
+    "url": "/React-app-first/index.html"
+  },
+  {
+    "revision": "30ebdabd4793d65c8a07",
+    "url": "/React-app-first/static/css/main.43bf5540.chunk.css"
+  },
+  {
+    "revision": "722fbf3a31a441f5f5eb",
+    "url": "/React-app-first/static/js/2.610e0c96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-app-first/static/js/2.610e0c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ebdabd4793d65c8a07",
+    "url": "/React-app-first/static/js/main.c6be9b07.chunk.js"
+  },
+  {
+    "revision": "1f4af35922c16fd76f5b",
+    "url": "/React-app-first/static/js/runtime-main.6b380965.js"
+  }
+]);
